b84cbaadc8b489c74e12301e7a2cb448bca83fdb
Israel Afonso de Carvalho Neto <israel@G1555-MX7-e3210d44.localdomain>
2021-09-07 22:25:20 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: const <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+        ],
+      ),
+    );
