dd254beeb24caeda2f7fe71e2b92d04da675783a
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2021-09-07 22:44:23 -0300

    Resolucao do exercicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: const <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-        ],
-      ),
-    );
+void main() {
+  runApp(
+    Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Testando uma coluna dentro de outra',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        )
+      ],
+    ),
+  );
+}
