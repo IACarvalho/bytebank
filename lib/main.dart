c809ce29b87ec01be2e9aac4522937b63899fcdc
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2021-09-07 23:17:28 -0300

    Resolução do exercídio

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferencias'),
         ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // codigo da açao ao aperta o botao
+          },
+          child: Icon(Icons.add),
         ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Testando uma coluna dentro de outra',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        )
-      ],
+      ),
     ),
   );
 }
