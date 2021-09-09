924ff568ef01baf15a4aea2e331c7ae36b603dd9
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2021-09-09 00:33:44 -0300

    Resolução do exercídio

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('19846'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: Text('19846'),
-              ),
-            ),
-          ],
-        ),
-        appBar: AppBar(
-          title: Text('Transferencias'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // codigo da açao ao aperta o botao
-          },
-          child: Icon(Icons.add),
-        ),
+        body: FormularioTransferencia(),
       ),
-    ),
-  );
+    );
+  }
+}
+
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando Transferencia'),
+      ),
+      body: Text('Teste'),
+    );
+  }
+}
+
+
+class ListaTransferencias extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferencias'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(300.0, 12345)),
+          ItemTransferencia(Transferencia(500.0, 127895)),
+          ItemTransferencia(Transferencia(100.0, 167435)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // codigo da açao ao aperta o botao
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+  
+}
+
+class Transferencia {
+
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
